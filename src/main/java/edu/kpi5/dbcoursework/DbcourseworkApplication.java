36805674590b75c36471477fdea7b41284e7128e
Tech phoENIX <zaritskiy.kirill@gmail.com>
2022-12-28 17:31:40 +0200

    Initial commit

---
+++ src/main/java/edu/kpi5/dbcoursework/DbcourseworkApplication.java
@@ -0,0 +1,13 @@
+package edu.kpi5.dbcoursework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbcourseworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbcourseworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/kpi5/dbcoursework/DbcourseworkApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.kpi5.dbcoursework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbcourseworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
