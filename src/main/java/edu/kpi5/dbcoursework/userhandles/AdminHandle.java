0c32be1beb23cd5c698d10ff77934e773c3a500a
ladisLAus2 <koliadiuk_vlad@ukr.net>
2023-01-03 14:57:20 +0200

    edited handles and left comments

--- src/main/java/edu/kpi5/dbcoursework/userhandles/AdminHandle.java
+++ src/main/java/edu/kpi5/dbcoursework/userhandles/AdminHandle.java
@@ -1,49 +1,106 @@
 package edu.kpi5.dbcoursework.userhandles;
 
-import edu.kpi5.dbcoursework.entities.AccessLevel;
-import edu.kpi5.dbcoursework.entities.Student;
-import edu.kpi5.dbcoursework.entities.User;
+import edu.kpi5.dbcoursework.dbaccess.DBApi;
+import edu.kpi5.dbcoursework.entities.*;
 import org.springframework.data.util.Pair;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class AdminHandle extends TeacherHandle{
     public AdminHandle(User user) {
         super(user);
     }
 
-    public ArrayList<Student> kickStudents(boolean deleteAcounts) {
+    public ArrayList<Student> kickStudents(boolean deleteAcounts)  {
+        //як саме повинна працювати дана функція ?
         return null;
     }
-    public int removeUsers(ArrayList<String> userNames) {
+    public int removeUsers(ArrayList<String> userNames, DBApi object) {
+        //чому повертається значення інт ?
+        object.removeUsers(userNames);
         return 0;
     }
-    public int applyScholarship(ArrayList<Student> listOfStudents, boolean increased) {
+    public int applyScholarship(ArrayList<Student> listOfStudents, boolean increased, DBApi object) {
+        //чому повертається інт ?
+        object.applyScholarship(listOfStudents, increased);
         return 0;
     }
-    public ArrayList<Student> getScholarshipList(boolean increased) {
-        return null;
+    public ArrayList<Student> getScholarshipList(boolean increased, DBApi object) {
+        return new ArrayList<>(object.getScholarshipList(increased));
     }
-    public boolean createUser(String userName, AccessLevel level, String password) {
+    public boolean createUser(String userName, AccessLevel level, String password, DBApi object) {
+        if(object.createUser(userName,level,password) == true){
+            return true;
+        }
         return false;
     }
-    public boolean editUser(String userName, AccessLevel level, String password) {
+    public boolean editUser(String userName, AccessLevel level, String password, DBApi object) {
+        if(object.editUser(userName,level,password) == true){
+            return true;
+        }
         return false;
     }
     public ArrayList<Pair<String, ArrayList<Pair<Float, Float>>>> getTeachersContribution() {
+        // для чого використовується ця функція ?
         return null;
     }
-    public User getUser(String userName) {
-        return null;
+    public User getUser(String userName, DBApi object) {
+        return object.getUser(userName);
     }
-    public ArrayList<User> getUserList() {
-        return null;
+    public ArrayList<User> getUserList(DBApi object) {
+        return new ArrayList<>(object.getUserList());
     }
-    public boolean addGroup(String groupName) {
+    public boolean addGroup(String groupName, DBApi object) {
+        try{
+            object.addGroup(groupName);
+            return true;
+        }catch (Exception e){
+
+        }
         return false;
     }
-    public boolean editGroup(String groupName) {
+    public boolean editGroup(String groupName, DBApi object) {
+        try{
+            object.editGroup(groupName);
+            return true;
+        }catch (Exception e){
+
+        }
         return false;
     }
-    //to continue
+
+    public boolean addStudentsToGroup(String groupName, ArrayList<Student> students, DBApi object){
+        var studentNames = new ArrayList<String>();
+        for(var a : students)
+            studentNames.add(a.getLogin());
+        if(object.addStudentsToGroup(groupName, studentNames)) {
+            return true;
+        }
+        return false;
+
+    }
+
+    public boolean removeGroup(String groupName, DBApi object){
+        //в DBApi в методі removeGroup йде видалення за айді, а не за назвою
+        object.removeGroup(groupName);
+    }
+
+    public Group getGroup(String groupName, DBApi object){
+        //в DBApi getGroup повинно повертати не список, а лише одну групу
+        return object.getGroup(groupName);
+    }
+
+    public ArrayList<Group> getGroupList(DBApi object){
+        return new ArrayList<>(object.getGroupList());
+    }
+
+    public List<Student> getAllStudents(DBApi object){
+        return object.getAllStudents();
+    }
+
+    public List<Teacher> getAllTeachers(DBApi object){
+        return object.getAllTeachers();
+    }
 }
--- src/main/java/edu/kpi5/dbcoursework/userhandles/TeacherHandle.java
+++ src/main/java/edu/kpi5/dbcoursework/userhandles/TeacherHandle.java
@@ -1,10 +1,12 @@
 package edu.kpi5.dbcoursework.userhandles;
 
+import edu.kpi5.dbcoursework.dbaccess.DBApi;
 import edu.kpi5.dbcoursework.entities.Course;
 import edu.kpi5.dbcoursework.entities.Student;
 import edu.kpi5.dbcoursework.entities.User;
 import edu.kpi5.dbcoursework.utility.MarksList;
 
+import java.util.AbstractMap;
 import java.util.ArrayList;
 
 public class TeacherHandle extends Handle {
@@ -12,47 +14,97 @@ public class TeacherHandle extends Handle {
         super(user);
     }
 
-    public int setAttestation(String courseName, MarksList marksList) {
-        return 0;
+    public void setAttestation(String courseName, MarksList marksList, DBApi object) {
+        //потрібно знати за яким правилом виставлятиметься атестація, наприклад якщо більше 20 балів
+        //але за що? ключ в марксЛісті містить тільки назву роботи, тому ми будемо тільки враховуючи її
+        // виставляти атестацію ?
+        for(var pair : marksList.getList()){//отримуємо список пар учень - оцінка
+            if(pair.getValue() > 20){
+                object.setAttestation(courseName,pair.getKey());//треба додати метод до BDApi для виставлення атестації учню за ім'ям
+            }
+        }
     }
 
-    public int setMark(String courseName, Student Student, int mark) {
-        return 0;
+    public void setMark(String courseName, Student Student, int mark, DBApi object) {
+        //поставив войд, бо чи потрібно щось повертати ?
+        try{
+            //переписати, бо непевний чи правильно розташував ключі
+            object.setMarks(courseName, new MarksList(courseName, new AbstractMap.SimpleEntry<String, Float>(Student.getName(),Float.valueOf(mark))));
+        }catch (Exception e){
+            //error handling
+        }
     }
 
-    public int setMarks(String courseName, MarksList marksList) {
-        return 0;
+    public void setMarks(String courseName, MarksList marksList, DBApi object) {
+        //поставив войд, бо чи потрібно щось повертати ?
+        try{
+            object.setMarks(courseName, marksList);
+        }catch (Exception e){
+            //error handling
+        }
     }
 
-    public int setSocialWork(String courseName, MarksList marksList) {
-        return 0;
+    public void setSocialWork(String courseName, MarksList marksList, DBApi object) {
+        //поставив войд, бо чи потрібно щось повертати ?
+        try{
+            object.setSocialWork(courseName, marksList);
+        }catch (Exception e){
+            //error handling
+        }
     }
 
-    public int setExam(String courseName, MarksList marksList) {
-        return 0;
+    public void setExam(String courseName, MarksList marksList, DBApi object) {
+        //поставив войд, бо чи потрібно щось повертати ?
+        try{
+            object.setExam(courseName, marksList);
+        }catch (Exception e){
+            //error handling
+        }
     }
 
-    public boolean addCourse(String courseName, ArrayList<String> groups) {
+    public void addCourse(String courseName, ArrayList<String> groups,DBApi object) {
+        //поставив войд, бо чи потрібно щось повертати ?
+        try{
+            object.addCourse(courseName,groups);
+        }catch (Exception e){
+            //error handling
+        }
+    }
+
+    public boolean editCourse(String courseName, String newCourseName, DBApi object) {
+        //чи потрібно щось повертати ?
+        try{
+            object.editCourse(courseName,newCourseName);
+            return true;
+        }catch (Exception e){
+            //error handling
+        }
         return false;
     }
 
-    public boolean editCourse(String courseName, String newCourseName) {
+    public boolean removeCourse(Long courseID, DBApi object) {
+        //в класі курсу немає courseID
+        //навіщо повертати булл ?
+        try{
+            object.removeCourse(courseID);
+            return true;
+        }catch (Exception e){
+            //error handling
+        }
         return false;
     }
 
-    public boolean removeCourse(String courseName) {
-        return false;
+    public Course getCourse(String courseName, DBApi object) {
+        return object.getCourse(courseName);//чому getCourse в DBApi повертає список, якщо повинен
+                                            //тільки один курс ?
+                                            //чи реалізувати пошук по списку в цьому методі ?
     }
 
-    public Course getCourse(String courseName) {
-        return null;
+    public ArrayList<Student> getCourseStudents(String courseName, DBApi object) {
+        return new ArrayList<>(object.getCourseStudents(courseName));
     }
 
-    public ArrayList<Student> getCourseStudents(String courseName) {
-        return null;
-    }
-
-    public ArrayList<Course> getCourseList() {
-        return null;
+    public ArrayList<Course> getCourseList(DBApi object) {
+        return new ArrayList<>(object.getCourseList(super.getUser()));
     }
 }
