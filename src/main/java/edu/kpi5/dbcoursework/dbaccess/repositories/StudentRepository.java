8f8b8819cff6bd05819d4542737635758b9d4045
AnriiAndrushko <andry.i.andrushko@gmail.com>
2023-01-06 00:46:46 +0200

    Implemented getScholarshipList query

--- src/main/java/edu/kpi5/dbcoursework/dbaccess/repositories/StudentRepository.java
+++ src/main/java/edu/kpi5/dbcoursework/dbaccess/repositories/StudentRepository.java
@@ -17,7 +17,36 @@ public interface StudentRepository extends CrudRepository<Student,Long> {
     List<Student> getKickList();
     //wrong query
     @Query(
-            value = "SELECT * FROM USERS u",
+            value = """
+                    SELECT s.*
+                    FROM students s
+                        LEFT JOIN(
+                        SELECT s.s_id AS i, COUNT(m.mark) AS c
+                        FROM students s
+                            JOIN marks m ON m.s_id = s.s_id
+                            GROUP BY s.s_id) all_marks
+                        ON all_marks.i = s.s_id
+                          
+                        LEFT JOIN (
+                            SELECT s.s_id AS i, COUNT(m.mark) AS c
+                            FROM students s
+                            JOIN marks m ON m.s_id = s.s_id
+                            WHERE m.mark = 5 OR m.mark = 4
+                            GROUP BY s.s_id) all_good_marks
+                        ON  all_good_marks.i = s.s_id
+                       
+                        LEFT JOIN (
+                            SELECT s.s_id AS i, COUNT(m.mark) AS c
+                            FROM students s
+                            JOIN marks m ON m.s_id = s.s_id
+                            WHERE m.mark = 3
+                            GROUP BY s.s_id) all_marks3
+                        ON  all_marks3.i = s.s_id
+                       
+                        WHERE all_marks.c = all_good_marks.c OR
+                        (
+                        all_marks.c = (all_good_marks.c + 1) AND
+                        all_marks3.c = 1);""",
             nativeQuery = true)
     List<Student> getScholarshipList();
     @Query(
