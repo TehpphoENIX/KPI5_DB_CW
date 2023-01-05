486b153c67fb33b3225c15e111d072b7087ace12
Tech phoENIX <zaritskiy.kirill@gmail.com>
2023-01-05 12:07:39 +0200

    Fixed som relationships. Added MongoDB entity

--- src/main/java/edu/kpi5/dbcoursework/dbaccess/marksdb/StudentCourseMarksRepository.java
+++
@@ -1,7 +0,0 @@
-package edu.kpi5.dbcoursework.dbaccess.marksdb;
-
-import edu.kpi5.dbcoursework.entities.marksdb.StudentCourseMarks;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface StudentCourseMarksRepository extends MongoRepository<StudentCourseMarks, String> {
-}
