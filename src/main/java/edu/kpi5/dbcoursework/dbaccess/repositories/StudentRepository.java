de69d7e0cc5a3ce0cab9940bd31505e67b0887b7
AnriiAndrushko <andry.i.andrushko@gmail.com>
2023-01-05 17:19:03 +0200

    Implemented getIncreasedScholarshipList query

--- src/main/java/edu/kpi5/dbcoursework/dbaccess/repositories/StudentRepository.java
+++ src/main/java/edu/kpi5/dbcoursework/dbaccess/repositories/StudentRepository.java
@@ -21,7 +21,19 @@ public interface StudentRepository extends CrudRepository<Student,Long> {
             nativeQuery = true)
     List<Student> getScholarshipList();
     @Query(
-            value = "SELECT * FROM USERS u",
+            value = "SELECT s.name, all_good_marks.c, all_marks.c\n" +
+                    "FROM students s, (\n" +
+                    "    SELECT s.s_id AS i, COUNT(m.mark) AS c\n" +
+                    "FROM students s\n" +
+                    "    JOIN marks m ON m.s_id = s.s_id\n" +
+                    "    GROUP BY s.s_id) all_marks,\n" +
+                    "    (\n" +
+                    "    SELECT s.s_id AS i, COUNT(m.mark) AS c\n" +
+                    "FROM students s\n" +
+                    "    JOIN marks m ON m.s_id = s.s_id\n" +
+                    "    WHERE m.mark = 5\n" +
+                    "    GROUP BY s.s_id) all_good_marks\n" +
+                    "    WHERE all_marks.c = all_good_marks.c AND all_marks.i = s.s_id AND all_marks.i = all_good_marks.i;",
             nativeQuery = true)
     List<Student> getIncreasedScholarshipList();
     List<Student> findAll();
