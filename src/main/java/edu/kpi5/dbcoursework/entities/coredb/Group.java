2c49931871b87400cdeffa747d4036edf2be6812
TehpphoENIX <91074010+TehpphoENIX@users.noreply.github.com>
2023-01-10 09:16:38 +0200

    Update Group.java

--- src/main/java/edu/kpi5/dbcoursework/entities/coredb/Group.java
+++ src/main/java/edu/kpi5/dbcoursework/entities/coredb/Group.java
@@ -26,7 +26,7 @@ public class Group {
 	@Column(name = "group_spec", nullable = false)
 	private Short speciality;
 
-	@ManyToOne(cascade = {CascadeType.ALL})
+	@ManyToOne
 	@JoinColumn(name="group_department")
 	private Department department;
 
