ecacb258ad7b2e41cf307c172406814fa292f1d9
The_sky_of_tears <theskyoftears13@gmail.com>
2023-01-10 06:36:21 +0200

    CourseRepTest

--- src/test/java/edu/kpi5/dbcoursework/dbaccess/coredb/CourseRepositoryTest.java
+++ src/test/java/edu/kpi5/dbcoursework/dbaccess/coredb/CourseRepositoryTest.java
@@ -1,13 +1,15 @@
 package edu.kpi5.dbcoursework.dbaccess.coredb;
 
-import edu.kpi5.dbcoursework.entities.coredb.Course;
-import org.junit.jupiter.api.AfterEach;
+import edu.kpi5.dbcoursework.entities.coredb.*;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,19 +18,31 @@ class CourseRepositoryTest {
 
     @Autowired
     private CourseRepository underTest;
+    @Autowired
+    private StudentRepository studentRep;
+    @Autowired
+    private TeacherRepository teacherRep;
+    @Autowired
+    private SCMRepository SCMRepo;
 
-    @AfterEach
-    public void clean(){
+    @AfterAll
+    public void clean() {
         underTest.deleteAll();
+        studentRep.deleteAll();
+        teacherRep.deleteAll();
+        SCMRepo.deleteAll();
     }
 
     @Test
     void findAll() {
+
         //given
         ArrayList<Course> courses = new ArrayList<>();
+
         for (int i = 0; i < 10; i++) {
             courses.add(new Course(String.valueOf(i)));
         }
+
         underTest.saveAll(courses);
         //when
         var result = underTest.findAll();
@@ -37,18 +51,84 @@ class CourseRepositoryTest {
     }
 
     @Test
-    void findByName() {
+    void findById() {
+
+        Course course = new Course("course1");
+
+        underTest.save(course);
+
+        Optional<Course> result = underTest.findById(course.getId());
+
+        result.ifPresent(value -> assertEquals(value.getId(), course.getId()));
     }
 
     @Test
     void findAllByStudentLogin() {
+
+        Student student = new Student("login", "name", "surname",
+                new Group(), 0, (short) 0, 0.0F, false);
+
+        studentRep.save(student);
+
+        List<Course> courses = new ArrayList<>();
+        courses.add(new Course("course1"));
+        courses.add(new Course("course2"));
+
+        underTest.saveAll(courses);
+
+        List<StudentCourseMarks> studMarks = new ArrayList<>();
+        studMarks.add( new StudentCourseMarks(student, courses.get(0)));
+        studMarks.add( new StudentCourseMarks(student, courses.get(1)));
+
+        SCMRepo.saveAll(studMarks);
+
+        var result = underTest.findAllByStudentLogin(student.getLogin());
+
+        assertIterableEquals(courses,result);
     }
 
     @Test
     void findAllByTeacherLogin() {
+
+        List<Course> courses = new ArrayList<>();
+        courses.add(new Course("course1"));
+        courses.add(new Course("course2"));
+
+        underTest.saveAll(courses);
+
+        Teacher teacher = new Teacher("login", "name", "surname", new Department());
+        teacher.setCourses((Set<Course>) courses);
+
+        teacherRep.save(teacher);
+
+        var result = underTest.findAllByTeacherLogin(teacher.getLogin());
+
+        assertIterableEquals(courses,result);
     }
 
     @Test
     void getStudentsByCourse() {
+
+        List<Student> students = new ArrayList<>();
+        students.add(new Student("login1", "name1", "surname1",
+                new Group(), 0, (short) 0, 0.0F, false));
+        students.add(new Student("login2", "name2", "surname2",
+                new Group(), 0, (short) 0, 0.0F, false));
+
+        studentRep.saveAll(students);
+
+        Course course = new Course("course");
+
+        underTest.save(course);
+
+        List<StudentCourseMarks> studMarks = new ArrayList<>();
+        studMarks.add( new StudentCourseMarks(students.get(0), course));
+        studMarks.add( new StudentCourseMarks(students.get(1), course));
+
+        SCMRepo.saveAll(studMarks);
+
+        var result = underTest.getStudentsByCourse(course.getId());
+
+        assertIterableEquals(students, result);
     }
 }
